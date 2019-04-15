e83d5afb15270932fd13c0fcdf760b0e538453ea
Du Heng <dh1992@tju.edu.cn>
2019-04-15 16:02:40 +0800

    'init'

---
+++ config/dev.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    NODE_ENV: '"development"'
+  },
+  defineConstants: {
+  },
+  weapp: {},
+  h5: {
+    devServer: {
+      open: false,
+      port: 10087
+    }
+  }
+}
